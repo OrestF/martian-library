ba46c956cf827d7bf2b6a6b53080076bb0f3dc12
Orest Falchuk <orestf@Orests-MacBook-Pro.local>
2021-04-10 16:53:55 +0300

    Finish tutorial

--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -1,15 +1,17 @@
 module Mutations
   class UpdateItemMutation < Mutations::BaseMutation
+    argument :id, ID, required: true
     argument :attributes, Types::ItemAttributes, required: true
 
     field :item, Types::ItemType, null: true
     field :errors, Types::ValidationErrorsType, null: true
 
-    def resolve(attributes:)
+    def resolve(id:, attributes:)
       check_authentication!
 
       item = Item.find(id)
-      if item.update(attributes.to_h.merge(user: context[:current_user]))
+      if item.update(attributes.to_h)
+        MartianLibrarySchema.subscriptions.trigger('itemUpdated', {}, item)
         { item: item }
       else
         { errors: item.errors }
